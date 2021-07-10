25a8a68f48c4c39cf6fde7fcb2f2f5f19e432109
SaucyGames05 <saucygamesofficial@gmail.com>
2021-07-09 23:03:55 -0700

    Listed commands

---
+++ better-nsfw/BetterNSFW.py
@@ -0,0 +1,82 @@
+from redbot.core import commands
+
+class BetterNSFW(commands.Cog):
+
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	async def random(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def ass(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def ecchi(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def oral(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def paizuri(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def milf(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def neko(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def yuri(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def yaoi(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def bdsm(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def trap(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def monster(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def tentacles(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def futa(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def porn(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def femboyporn(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def porngif(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def r34(self, ctx):
+		# put shit here
+
+	@commands.command()
+	async def hentaigif(self, ctx):
+		# put shit here
---
+++ better-nsfw/__init__.py
@@ -0,0 +1,4 @@
+from .BetterNSFW import BetterNSFW
+
+def setup(bot):
+	bot.add_cog(BetterNSFW(bot))
